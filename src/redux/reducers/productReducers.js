63184c31e1be85b5d2ee17cad9b59779055f3653
farjana islam <nipa.farzana87@gmail.com>
2022-12-18 17:59:12 +0600

    working on add to cart

--- src/redux/reducers/productReducers.js
+++ src/redux/reducers/productReducers.js
@@ -5,13 +5,24 @@ const initialState = {
 };
 
 const productReducer = (state = initialState, action) => {
+    const selectedProduct = state.cart.find(
+        (product) => product.id === action.payload.id
+      );
     switch(action.type){
         case actionTypes.ADD_TO_CART: 
+        if(selectedProduct){
+            selectedProduct.quantity = selectedProduct.quantity + 1
             return {
                 ...state,
-                cart: [...state.cart, action.payload]
+                cart:[...state.cart, selectedProduct]
+            };
+        }
+            return {
+                ...state,
+                cart: [...state.cart, { ...action.payload, quantity: 1 }],
             }
         case actionTypes.REMOVE_FROM_CART:
+            console.log(action.payload.id);
             return {
                 ...state,
                 cart: state.cart.filter(product => product.id !== action.payload.id)
